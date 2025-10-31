dd87da4997c38210ce605f1c66d6e7c5672f14a5
Hari <hari09111999@gmail.com>
2025-10-31 12:34:24 +0530

    [Hari] | Refactor. controller endpoint type

--- multi_company_toggle_with_ref_field/controllers/controllers.py
+++ multi_company_toggle_with_ref_field/controllers/controllers.py
@@ -4,7 +4,7 @@ from odoo.http import request
 
 class MultiCompanyToggleWithRefField(http.Controller):
 
-    @http.route('/action_company_reference', methods=["POST"], type="json", auth="user")
+    @http.route('/action_company_reference', methods=["POST"], type="jsonrpc", auth="user")
     def action_company_reference(self, company_id):
         """ multi company reference endpoint"""
         company_record = request.env['res.company'].sudo().browse(company_id)
