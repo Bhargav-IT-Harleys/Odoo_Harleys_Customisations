51e57e83d47359a3ebd5d8f4353fe1af736c7fd8
Hari <hari09111999@gmail.com>
2025-11-14 15:03:37 +0530

    [Hari] | Add. multi company set gls logic in create function

--- multi_company_contacts_and_category_set_gls/models/res_partner.py
+++ multi_company_contacts_and_category_set_gls/models/res_partner.py
@@ -35,3 +35,42 @@ class ResPartner(models.Model):
                 target_partner.with_company(company).with_context(sync_accounts_done=True).write(sync_vals)
 
         return res
+
+    def create(self, vals):
+        if isinstance(vals, list):
+            partners = super().create(vals)
+            for partner, val in zip(partners, vals):
+                partner._sync_accounts_on_create(val)
+            return partners
+        else:
+            partner = super().create(vals)
+            partner._sync_accounts_on_create(vals)
+            return partner
+
+    def _sync_accounts_on_create(self, vals):
+        if self.env.context.get('sync_accounts_done'):
+            return
+
+        receivable_id = vals.get('property_account_receivable_id')
+        payable_id = vals.get('property_account_payable_id')
+
+        if not receivable_id and not payable_id:
+            return
+
+        companies = self.env['res.company'].search([])
+        for company in companies:
+            target_partner = self.with_company(company).search([
+                ('id', '=', self.id)
+            ], limit=1)
+
+            if not target_partner:
+                continue
+
+            sync_vals = {}
+            if receivable_id is not None:
+                sync_vals['property_account_receivable_id'] = receivable_id
+            if payable_id is not None:
+                sync_vals['property_account_payable_id'] = payable_id
+
+            target_partner.with_company(company).with_context(sync_accounts_done=True).write(sync_vals)
+
