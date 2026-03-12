59a36e3930d9517774debd473cb6bcbb0ecabee5
Hari <hari09111999@gmail.com>
2026-03-12 10:22:18 +0530

    [hari] | Refactor. payslip download issue.

--- employee_portal/models/hr_payslip.py
+++ employee_portal/models/hr_payslip.py
@@ -7,5 +7,5 @@ class HrPayslip(models.Model):
     wage_type = fields.Selection(related="version_id.wage_type", groups="base.group_portal")
 
     def action_print_payslip(self):
-        ...
-
+        report = self.env['ir.actions.report']._get_report_from_name('hr_payroll.report_payslip_lang')
+        return report.report_action(self)
