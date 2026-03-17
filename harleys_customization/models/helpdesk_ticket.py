c569aafcbc3963fc2ec514beeac78d0f22ca5631
Hari <hari09111999@gmail.com>
2026-03-17 19:51:42 +0530

    [Hari] |Fix. helpdesk related issues

--- harleys_customization/models/helpdesk_ticket.py
+++ harleys_customization/models/helpdesk_ticket.py
@@ -13,28 +13,11 @@ class HelpdeskTicket(models.Model):
     department_id = fields.Many2one('hr.department', "Department", required=True)
     location_id = fields.Many2one('stock.warehouse', "Location", required=True)
     service_type_id = fields.Many2one('service.type', "Service Type", required=True)
-    partner_id = fields.Many2one('res.partner', string='Customer', tracking=True, index=True)
-
     priority = fields.Selection(TICKET_PRIORITY, string='Priority', default='0',readonly=True, tracking=True, compute='_compute_priority', store=True)
 
     @api.depends('service_type_id')
     def _compute_priority(self):
         if self.service_type_id:
             self.priority = self.service_type_id.priority
-
-    @api.depends('ticket_ref', 'partner_name')
-    @api.depends_context('with_partner')
-    def _compute_display_name(self):
-        display_partner_name = self.env.context.get('with_partner', False)
-        ticket_with_name = self.filtered('name')
-        for ticket in ticket_with_name:
-            name = ticket.name
-            if ticket.ticket_ref:
-                name += f' (#{ticket.ticket_ref})'
-            if display_partner_name and ticket.partner_name:
-                name += f' - {ticket.partner_name}'
-            ticket.display_name = name
-            ticket.partner_id = self.env['res.partner'].sudo().search([('name', '=', ticket.partner_name)], limit=1).id
-        return super(HelpdeskTicket, self - ticket_with_name)._compute_display_name()
 
     
