a64685384401b2a356812797c3106b83b1c16779
Hari <hari09111999@gmail.com>
2026-04-30 18:35:09 +0530

    [Hari] | Refactor. MO Material Requirments record rule

--- user_warehouse_restriction/__manifest__.py
+++ user_warehouse_restriction/__manifest__.py
@@ -26,13 +26,14 @@
     'description': """This module helps you to restrict warehouse and stock 
      location for the specific users. So that users can only access the allowed
      warehouse and locations.""",
-    'depends': ['stock_sms'],
+    'depends': ['stock_sms', 'harleys_customization'],
     'data': [
         'security/user_warehouse_restriction_groups.xml',
         'security/user_warehouse_restriction_security.xml',
         'views/res_config_settings_views.xml',
         'views/stock_warehouse_views.xml',
         'views/res_users_views.xml',
+        'views/mrp_production_views.xml',
     ],
     'images': ['static/description/banner.jpg'],
     'application': False,
