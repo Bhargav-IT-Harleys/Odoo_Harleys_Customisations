85ccdb8aed5ea662a917d7a0047b1270b0ca0b54
Hari <hari09111999@gmail.com>
2026-04-30 18:52:23 +0530

    [Hari] | Refactor. Validation: Selected Delivery From is not same

--- harleys_customization/models/indent_request.py
+++ harleys_customization/models/indent_request.py
@@ -496,8 +496,8 @@ class IndentRequestLine(models.Model):
 
         delivery_from_values = selected_lines.mapped('delivery_from')
         if len(delivery_from_values) > 1:
-            raise UserError(_("Select Delivery From not same!"))
-            
+            raise UserError(_("Selected Delivery From is not same"))
+
         return {
             'type': 'ir.actions.act_window',
             'name': 'Create Draft MO',
