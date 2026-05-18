7d70672f3b1f0858d2623ffa7e1542b1a97c9f7d
Hari <hari09111999@gmail.com>
2026-05-18 15:59:53 +0530

    [Hari] | Add. Menu groups for the physical-inventory in inventory

--- harleys_customization/__manifest__.py
+++ harleys_customization/__manifest__.py
@@ -27,6 +27,7 @@
         'views/helpdesk_portal_template.xml',
         'views/purchase_views.xml',
         'views/stock_move_views.xml',
+        'views/stock_quant_views.xml',
         'reports/manufacturing_order_lines_views.xml',
         'wizard/mol_internal_transfer_views.xml',
         'wizard/mo_internal_transfer_views.xml',
