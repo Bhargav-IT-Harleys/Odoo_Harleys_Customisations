6a0e54604351b0161f1349b130aadf547515b856
Aspireal-Bhargav <bhargava@aspial.com>
2026-08-03 19:04:57 +0530

    added the constrain for the bill reference, in the physical inventory made the user as default logged user, and in the PO made the send RFQ as invisible

--- harleys_customization/models/purchase.py
+++ harleys_customization/models/purchase.py
@@ -1,7 +1,41 @@
 from odoo import models, fields, api, _
-
+from odoo.exceptions import ValidationError
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     purchase_type = fields.Selection([('x_p_o', 'Purchase Order (External PO)'), ('i_p_o', 'Internal Transfer (Internal PO)')], string='Purchase type')
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    @api.constrains('ref', 'partner_id', 'move_type')
+    def _check_unique_ref_per_vendor(self):
+        for bill in self:
+            if (
+                bill.move_type != 'in_invoice'
+                or not bill.ref
+                or not bill.partner_id
+            ):
+                continue
+            duplicate_bills = self.sudo().search([
+                ('id', '!=', bill.id),
+                ('move_type', '=', 'in_invoice'),
+                ('partner_id', '=', bill.partner_id.id),
+                ('ref', '=', bill.ref),
+            ])
+            if duplicate_bills:
+                bill_names = ", ".join(
+                    duplicate_bills.mapped("display_name")
+                )
+                raise ValidationError(_(
+                    "The Vendor Bill Reference '%(ref)s' has already been used "
+                    "for vendor '%(vendor)s'.\n\n"
+                    "Company: %(company)s\n"
+                    "Existing Bill(s): %(bills)s"
+                ) % {
+                    'ref': bill.ref,
+                    'vendor': bill.partner_id.display_name,
+                    'company': duplicate_bills[0].company_id.display_name,
+                    'bills': bill_names,
+                })
--- harleys_customization/models/stock_picking.py
+++ harleys_customization/models/stock_picking.py
@@ -141,6 +141,7 @@ class StockScrap(models.Model):
         default=False
     )
 
+<<<<<<< Updated upstream
     @api.model
     def action_view_inv_adjustments(self):
         """Open Inv Adjustments (stock.action_stock_scrap) restricted to
@@ -204,3 +205,13 @@ class StockScrap(models.Model):
             if isinstance(result, dict):
                 return result
         return {'type': 'ir.actions.act_window_close'}
+=======
+class StockQuant(models.Model):
+    _inherit = "stock.quant"
+
+    user_id = fields.Many2one(
+        "res.users",
+        string="User",
+        default=lambda self: self.env.user,
+    )
+>>>>>>> Stashed changes
